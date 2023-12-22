d1abb7e03cd2607b13af67ac9f30d254f65b1fbe
brijesh <brijeshkanpariya25@gmail.com>
2023-12-22 17:29:40 +0530

    all set

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 const dbUrl = process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
   .then(() => {
@@ -30,7 +31,7 @@ main()
     console.log(err);
   });
 async function main() {
-  await mongoose.connect(dbUrl);
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
